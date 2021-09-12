30117a6f4ee97ccdc0fc35864d4c8287d47f5036
nNabakhteveli <nabakhtevelin@gmail.com>
2021-09-13 00:35:36 +0400

    Expanded image resolution to 2048x2048 and added download functionality

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
-
 window.addEventListener('load', () => {
   document.getElementById('imageUploadInput').addEventListener('change', function () {
     if (this.files && this.files[0]) {
-      let img = document.getElementById('img');
-      img.onload = () => {
-        URL.revokeObjectURL(img.src);
+      let previewImg = document.getElementById('img');
+      previewImg.onload = () => {
+        URL.revokeObjectURL(previewImg.src);
+        previewImg.src = URL.createObjectURL(this.files[0]);
       }
-
-      img.src = URL.createObjectURL(this.files[0]);
 
       const canvas = document.getElementById("canvas");
       const ctx = canvas.getContext("2d");
       let originalImg = new Image();
       let frameImg = new Image();
+
       originalImg.src = URL.createObjectURL(this.files[0]);
 
       originalImg.onload = () => {
-        let w = 250, h = 150;
-        ctx.drawImage(originalImg, 0, 0, w, h);
-        frameImg.src = "./img/frame.png";
-        frameImg.onload = () => {
-          ctx.drawImage(frameImg, 0, 0, w, h);
-
-          let img = canvas.toDataURL("image/png");
-          document.write('<img src="' + img + '" width="200" height="200"/>');
+        try {
+          const w = canvas.width, h = canvas.height;
+          ctx.drawImage(originalImg, 0, 0, w, h);
+          frameImg.src = "./img/frame.png";
+          frameImg.onload = () => {
+            ctx.drawImage(frameImg, 0, 0, w, h);
+            
+            let img = canvas.toDataURL("image/jpg", 1.0);
+            document.write(`<img src="${img}" width="200" height="150"/><a href="${img}" download><button>გადმოწერა</button></a>`);
+          }
+        } catch(err) {
+          console.log("Something went wrong:\n", err);
         }
       }
     }
