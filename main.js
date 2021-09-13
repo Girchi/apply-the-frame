36dae58f90c0cbe17d4b6bd57312f1a5ebc8e5df
nNabakhteveli <nabakhtevelin@gmail.com>
2021-09-13 20:15:22 +0400

    Fixed image width resizing bug

--- main.js
+++ main.js
@@ -12,40 +12,51 @@ window.addEventListener('load', () => {
       let originalImg = new Image();
       let frameImg = new Image();
 
-      originalImg.src = URL.createObjectURL(this.files[0]);
-
-      originalImg.onload = () => {
+      originalImg.src = URL.createObjectURL(this.files[0]);      
+      
+      originalImg.addEventListener("load", () => {
         try {
-          const w = canvas.width, h = canvas.height;
-          ctx.drawImage(originalImg, 0, 0, w, h);
+          const w = canvas.width, h = canvas.height, imgW = originalImg.width;
+          if(imgW > 1000) {
+              ctx.drawImage(originalImg, 0, 0, w, h);
+          } else if (imgW > 700 && imgW <= 720){
+            ctx.drawImage(originalImg, (imgW - (w / 2)) / 2, 0);
+          } else {
+            ctx.drawImage(originalImg, 0, 0, w, h);
+          }
           frameImg.src = "./img/frame.png";
           frameImg.onload = () => {
             ctx.drawImage(frameImg, 0, 0, w, h);
             
-            let img = canvas.toDataURL("image/jpg", 1.0);
+            let img = canvas.toDataURL("image/png", 1.0);
             document.write(`
             <style>
-              .generatedContainer {
+            .generatedContainer {
                 align-items: center;
                 margin: 180px auto;
                 width: max-content;
+              }
+              .generatedImg {
+                width: 200px;
+                height: 150px;
               }
               p {
                 font-family: sans-serif;
               }
             </style>
+            
             <div class='generatedContainer'>
-              <p>დააკლიკე გადმოსაწერად</p>
+            <p>დააკლიკე გადმოსაწერად</p>
               <a href="${img}" download>
-                <img class='generatedImg' src="${img}" width="200" height="150"/>
-              </a>
+                <img class='generatedImg' src="${img}">
+                </a>
             </div>
             `);
           }
         } catch(err) {
           console.log("Something went wrong:\n", err);
         }
-      }
+      })
     }
   });
 });
@@ -57,4 +68,4 @@ canvas = document.querySelector("#canvas");
 selectResolution.addEventListener("change", () => {
   canvas.width = selectResolution.value;
   canvas.height = selectResolution.value;
-})
+});
