da305505e5bb3d0ae29c576417cc6e0aa1b1c2f9
nNabakhteveli <nabakhtevelin@gmail.com>
2021-09-14 20:34:15 +0400

    Fixed image stretching bug for images with over 1500 height

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('load', () => {
   document.getElementById('imageUploadInput').addEventListener('change', function () {
     if (this.files && this.files[0]) {
@@ -11,19 +12,64 @@ window.addEventListener('load', () => {
       const ctx = canvas.getContext("2d");
       let originalImg = new Image();
       let frameImg = new Image();
+      
+      originalImg.src = URL.createObjectURL(this.files[0]);
+      
+      function cropImage(url, aspectRatio) {
+        return new Promise((resolve) => {
+          const inputImage = new Image();
+      
+          inputImage.onload = () => {
+            const inputWidth = inputImage.naturalWidth,
+            inputHeight = inputImage.naturalHeight;
+      
+            // get the aspect ratio of the input image
+            const inputImageAspectRatio = inputWidth / inputHeight;
+      
+            // if it's bigger than our target aspect ratio
+            let outputWidth = inputWidth;
+            let outputHeight = inputHeight;
+            if (inputImageAspectRatio > aspectRatio) {
+              outputWidth = inputHeight * aspectRatio;
+            } else if (inputImageAspectRatio < aspectRatio) {
+              outputHeight = inputWidth / aspectRatio;
+            }
+      
+            // calculate the position to draw the image at
+            const outputX = (outputWidth - inputWidth) * 0.5;
+            const outputY = (outputHeight - inputHeight) * 0.5;
+      
+            // create a canvas that will present the output image
+            const outputImage = document.createElement("canvas");      
+            outputImage.width = outputWidth;
+            outputImage.height = outputHeight;
+            
+      
+            const ctx = outputImage.getContext("2d");
+            ctx.drawImage(inputImage, outputX, outputY);
 
-      originalImg.src = URL.createObjectURL(this.files[0]);      
+            resolve([ outputImage, inputImage ]);
+          };
+          inputImage.src = url;
+        });
+      }
+
       
       originalImg.addEventListener("load", () => {
         try {
           const w = canvas.width, h = canvas.height, imgW = originalImg.width;
-          if(imgW > 1000) {
-              ctx.drawImage(originalImg, 0, 0, w, h);
-          } else if (imgW > 700 && imgW <= 720){
+          if(originalImg.height > 1000) {
+            ctx.drawImage(originalImg, 0, 0, w, originalImg.height);
+            console.log(originalImg.height);
+          } else if(imgW > 1000) {
+            ctx.drawImage(originalImg, 0, 0, w, h);
+          }
+           else if (imgW > 700 && imgW <= 720){
             ctx.drawImage(originalImg, (imgW - (w / 2)) / 2, 0);
           } else {
             ctx.drawImage(originalImg, 0, 0, w, h);
           }
+
           frameImg.src = "./img/frame.png";
           frameImg.onload = () => {
             ctx.drawImage(frameImg, 0, 0, w, h);
@@ -37,8 +83,8 @@ window.addEventListener('load', () => {
                 width: max-content;
               }
               .generatedImg {
-                width: 200px;
-                height: 150px;
+                width: 300px;
+                height: 300px;
               }
               p {
                 font-family: sans-serif;
