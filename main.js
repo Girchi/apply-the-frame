a2ee7784087689f22de46770d8794fbd444af406
nNabakhteveli <nabakhtevelin@gmail.com>
2021-09-16 03:44:53 +0400

    Trying to force open link in the browser for mobile devices

--- main.js
+++ main.js
@@ -114,3 +114,16 @@ window.addEventListener('load', () => {
   })
   document.getElementById('imageUploadInput').value = null;
 })
+
+function openInBrowser(target, browserScheme) {
+  var ifc = document.createElement("div");
+  ifc.innerHTML = `<iframe src='${browserScheme}${target}' style='width:0;height:0;border:0; border:none;visibility: hidden;'></iframe>`;
+  document.body.appendChild(ifc);
+}
+
+function isInApp(appSpecificUserAgents) {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  for (i = 0; i <= appSpecificUserAgents.length; i++) {
+      if (userAgent.indexOf(appSpecificUserAgents[i]) > -1) return true;
+  }
+}
