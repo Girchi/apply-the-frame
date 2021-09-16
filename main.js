8e509833a09db5326af30b29aa1bf464d32c0ad8
nNabakhteveli <nabakhtevelin@gmail.com>
2021-09-16 04:26:12 +0400

    Trying to use dynamic link

--- main.js
+++ main.js
@@ -1,116 +1,119 @@
-import './croppie/croppie.js';
+window.location.replace("https://nnabakhteveli.github.io/apply-the-frame/")
 
-// Detect if user is using Safari browser
-let isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+// import './croppie/croppie.js';
 
-function handleCrop(imageSrc) {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-  let originalImg = new Image();
-  let frameImg = new Image();
+// // Detect if user is using Safari browser
+// let isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+// function handleCrop(imageSrc) {
+//   const canvas = document.getElementById("canvas");
+//   const ctx = canvas.getContext("2d");
+//   let originalImg = new Image();
+//   let frameImg = new Image();https://nnabakhteveli.github.io/apply-the-frame/
   
-  originalImg.src = imageSrc;
+//   originalImg.src = imageSrc;
   
-  originalImg.addEventListener("load", () => {
-    try {
-      const w = canvas.width, h = canvas.height;
-      ctx.drawImage(originalImg, 0, 0, w, h);
+//   originalImg.addEventListener("load", () => {
+//     try {
+//       const w = canvas.width, h = canvas.height;
+//       ctx.drawImage(originalImg, 0, 0, w, h);
 
-      frameImg.src = "./img/frame.png";
-      frameImg.onload = () => {
-        ctx.drawImage(frameImg, 0, 0, w, h);
+//       frameImg.src = "./img/frame.png";
+//       frameImg.onload = () => {
+//         ctx.drawImage(frameImg, 0, 0, w, h);
         
-        let img = canvas.toDataURL("image/png", 1.0);
+//         let img = canvas.toDataURL("image/png", 1.0);
 
-        if(isSafari) {
-          document.write(`
-          <style>
-          @import "./cropped.css"
-          </style>
+//         if(isSafari) {
+//           document.write(`
+//           <style>
+//           @import "./cropped.css"
+//           </style>
           
-          <div class='generatedContainer'>
-          <p>დააკლიკე ფოტოს გასახსნელად</p>
-            <a href="${img}" download="FramedImage">
-              <img class='generatedImg' src="${img}">
-              </a>
-          </div>
-          `);
-        } else {
-          document.write(`
-          <style>
-          @import "./cropped.css"
-          </style>
+//           <div class='generatedContainer'>
+//           <p>დააკლიკე ფოტოს გასახსნელად</p>
+//             <a href="${img}" download="FramedImage">
+//               <img class='generatedImg' src="${img}">
+//               </a>
+//           </div>
+//           `);
+//         } else {
+//           document.write(`
+//           <style>
+//           @import "./cropped.css"
+//           </style>
           
-          <div class='generatedContainer'>
-          <p>დააკლიკე ფოტოს გადმოსაწერად</p>
-            <a href="${img}" download>
-              <img class='generatedImg' src="${img}">
-              </a>
-          </div>
-          `);
-        }
+//           <div class='generatedContainer'>
+//           <p>დააკლიკე ფოტოს გადმოსაწერად</p>
+//             <a href="${img}" download>
+//               <img class='generatedImg' src="${img}">
+//               </a>
+//           </div>
+//           `);
+//         }
         
-      }
-    } catch(err) {
-      console.log("Something went wrong:\n", err);
-    }
-  })
-}
+//       }
+//     } catch(err) {
+//       console.log("Something went wrong:\n", err);
+//     }
+//   })
+// }
 
-// Manually changing image resolution. Default: 1000x1000
-const selectResolution = document.querySelector("#selectResolution"),
-canvas = document.querySelector("#canvas");
+// // Manually changing image resolution. Default: 1000x1000
+// const selectResolution = document.querySelector("#selectResolution"),
+// canvas = document.querySelector("#canvas");
 
-selectResolution.addEventListener("change", () => {
-  canvas.width = selectResolution.value;
-  canvas.height = selectResolution.value;
-});
+// selectResolution.addEventListener("change", () => {
+//   canvas.width = selectResolution.value;
+//   canvas.height = selectResolution.value;
+// });
 
-// Load croppie
-window.addEventListener('load', () => {
-  document.getElementById('imageUploadInput').addEventListener('change', function () {
-    if (this.files && this.files[0]) {
-      let previewImg = document.getElementById('img');
-      previewImg.addEventListener("load", () => {
-        // URL.revokeObjectURL(previewImg.src);
-        let imgDiv = document.getElementById('cropImage');
-        imgDiv.style.display = 'table';
-        imgDiv.style.margin = '0 auto';
-        imgDiv.style.textAlign = 'center';
+// // Load croppie
+// window.addEventListener('load', () => {
+//   document.getElementById('imageUploadInput').addEventListener('change', function () {
+//     if (this.files && this.files[0]) {
+//       let previewImg = document.getElementById('img');
+//       previewImg.addEventListener("load", () => {
+//         // URL.revokeObjectURL(previewImg.src);
+//         let imgDiv = document.getElementById('cropImage');
+//         imgDiv.style.display = 'table';
+//         imgDiv.style.margin = '0 auto';
+//         imgDiv.style.textAlign = 'center';
 
-        // Submit button
-        const btn = document.createElement("button");
-        btn.textContent = 'გენერაცია';
-        btn.id = 'btn';
+//         // Submit button
+//         const btn = document.createElement("button");
+//         btn.textContent = 'გენერაცია';
+//         btn.id = 'btn';
 
 
-        let croppie = new Croppie(imgDiv, {
-          enableExif: true,
-          viewport: { width: 320, height: 320, type: 'square' },
-          boundary: { width: 450, height: 450 }
-        });
+//         let croppie = new Croppie(imgDiv, {
+//           enableExif: true,
+//           viewport: { width: 320, height: 320, type: 'square' },
+//           boundary: { width: 450, height: 450 }
+//         });
 
-        croppie.bind({
-          url: previewImg.src
-        });
-        // Add Submit button
-        imgDiv.append(btn);
+//         croppie.bind({
+//           url: previewImg.src
+//         });
+//         // Add Submit button
+//         imgDiv.append(btn);
 
-        // Add a frame to the cropped image
-        document.getElementById('btn').addEventListener('click', () => {
+//         // Add a frame to the cropped image
+//         document.getElementById('btn').addEventListener('click', () => {
           
-          let imageSize = {
-            width: 1000,
-            height: 1000,
-            type: "square"
-          }
+//           let imageSize = {
+//             width: 1000,
+//             height: 1000,
+//             type: "square"
+//           }
 
-          croppie.result({type: 'canvas', size: imageSize, format: 'png', quality: 1, circle: false})
-          .then((src) => { handleCrop(src); });
-        })  
-      });
-      previewImg.src = URL.createObjectURL(this.files[0]);
-    }
-  })
-  document.getElementById('imageUploadInput').value = null;
-})
+//           croppie.result({type: 'canvas', size: imageSize, format: 'png', quality: 1, circle: false})
+//           .then((src) => { handleCrop(src); });
+//         })  
+//       });
+//       previewImg.src = URL.createObjectURL(this.files[0]);
+//     }
+//   })
+//   document.getElementById('imageUploadInput').value = null;
+// })
+
